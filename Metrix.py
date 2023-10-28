d2df98eb5e2bd6d82f5080cfd8f8555031e731e9
nethmi98jay <88782795+nethmi98jay@users.noreply.github.com>
2023-10-28 21:53:29 +0530

    Update Metrix.py

--- Metrix.py
+++ Metrix.py
@@ -12,29 +12,7 @@ This app predicts the proficiency percentage of an employee based on their skill
 data = pd.read_csv('employee_skill_dataset.csv')
 
 def calculate_proficiency(row):
-    programming_weight = 0.3
-    networking_weight = 0.2
-    db_management_weight = 0.1
-    communication_weight = 0.1
-    teamwork_weight = 0.1
-    emotional_intelligence_weight = 0.05
-    leadership_weight = 0.05
-    team_building_weight = 0.05
-    risk_management_weight = 0.05
-
-    proficiency_percentage = (
-        row['Programming_Skill'] * programming_weight +
-        row['Networking_Skill'] * networking_weight +
-        row['Database_Management'] * db_management_weight +
-        row['Communication'] * communication_weight +
-        row['Teamwork_Collaboration'] * teamwork_weight +
-        row['Emotional_Intelligence'] * emotional_intelligence_weight +
-        row['Leadership'] * leadership_weight +
-        row['Team_Building'] * team_building_weight +
-        row['Risk_Management'] * risk_management_weight
-    )
-
-    return proficiency_percentage
+    # same function as before
     
 data['Proficiency_Percentage'] = data.apply(calculate_proficiency, axis=1) 
 
@@ -47,9 +25,22 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Build model 
 regressor = RandomForestRegressor(n_estimators=100, random_state=42)
 regressor.fit(X_train, y_train)
+def get_unique_role():
+    get_role = set()
+    with open('employee.csv', mode='r') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            get_role.add(row[10])
+
+    return sorted(list(get_role))
 
 # Create sidebar for user input
 st.sidebar.header('Enter Employee Skills')
+st.sidebar.input('Enter Employee Name')
+st.sidebar.selectbox(
+    'Job Role',
+    (get_unique_role()))
 programming = st.sidebar.slider('Programming', 0, 100, 50)
 networking = st.sidebar.slider('Networking', 0, 100, 50) 
 db = st.sidebar.slider('Database Management', 0, 100, 50)
